9b3993ae06d23acb6a5b9c166f68d5580de7a93f
IMGoodrich <igoodrich@phase2technology.com>
2020-09-16 16:28:31 -0700

    docs: adds docs to four

--- packages/four/lib/four.js
+++ packages/four/lib/four.js
@@ -1,4 +1,5 @@
 'use strict';
+// Takes the the array from one and reduces it using three...then doubles it!! Nigh-on nefarious.
 const packageOne = require('../../one/lib/one');
 const packageThree = require('../../three/lib/three');
 
